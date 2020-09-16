9f0ac0f16c962e8b69e82d953894ffa677bf6c26
facudelucia <facundodeluciajudo@gmail.com>
2020-09-16 10:53:34 -0300

    agregado proyectos nuevos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,6 +27,30 @@ const Projects = () => {
                                 <a id="githubAnchor" className="card-text" href="https://github.com/facudelucia/judo-scoreboard" target="_blank">{english ? "Link to github repository" : "Link a repositorio de github"}</a>
                             </div>
                         </div>
+                        <div className="card">
+                            <img src={"./assets/amongusimg.png"} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Among Us Guide</h4>
+                                <a href="https://guiaamongus.netlify.app/" target="_blank"> <button
+                                    type="button"
+                                    className="btn btn-outline-primary btn-block"
+                                    /* onClick={()=>seeMore("https://judo-scoreboard.netlify.app/")} */
+                                >{english ? "See more..." : "Ver más..."}</button></a>
+                                <a id="githubAnchor" className="card-text" href="https://github.com/facudelucia/among-us-guide" target="_blank">{english ? "Link to github repository" : "Link a repositorio de github"}</a>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src={"./assets/waimg.png"} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">Whatsapp Clone</h4>
+                                <a href="https://facudeluciawhatsapp.netlify.app/" target="_blank"> <button
+                                    type="button"
+                                    className="btn btn-outline-primary btn-block"
+                                    /* onClick={()=>seeMore("https://judo-scoreboard.netlify.app/")} */
+                                >{english ? "See more..." : "Ver más..."}</button></a>
+                                <a id="githubAnchor" className="card-text" href="https://github.com/facudelucia/whatsapp" target="_blank">{english ? "Link to github repository" : "Link a repositorio de github"}</a>
+                            </div>
+                        </div>
                         
                     </div>
                 </div>
